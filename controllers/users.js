31c96716eace9fce15603f914a41bff9c16d1281
Brian Palmer <dreamsandcode@gmail.com>
2021-02-11 22:44:15 -0500

    simple crud examples for postman

--- controllers/users.js
+++ controllers/users.js
@@ -6,30 +6,112 @@ module.exports.controller = (app) => {
         res.render('users', { title: 'Users', description: 'This is the description of all the users' });
     })
 
-    app.post('/users/create', (req,res) => {
+    app.post('/users/create', (req, res) => {
         const user_resource = new User({
             name: 'Brian Palmer',
             email: 'dreamsandcode@gmail.com'
         })
 
         user_resource.save((error) => {
-            if(error)
+            if (error)
                 console.log(error);
-            
+
             res.send({
                 success: true,
                 code: 200,
-                msg: user_resource.name + ' was added' 
+                msg: user_resource.name + ' was added'
             })
         })
     })
 
-    app.post('/users/find', (req,res) => {
+    app.post('/users/findall', (req, res) => {
 
         User.find({}, 'name email', function (error, users) {
             if (error) { console.error(error); }
             res.send({
                 users: users
+            })
+        })
+    })
+
+    app.post('/users/fineone', (req, res) => {
+        User.findById(1, 'name email', function (error, user) {
+            if (error) { console.error(error); }
+            res.send(user)
+        })
+    })
+
+    app.post('users/findandsave', (req, res) => {
+        // 1 is the ID
+        User.findById(1, 'name email', function (error, user) {
+            if (error) { console.error(error); }
+            user.name = 'Peter'
+            user.email = 'peter@gmail.com'
+            user.save(function (error) {
+                if (error) {
+                    console.log(error)
+                }
+                res.send({
+                    success: true
+                })
+            })
+        })
+    })
+
+    app.post('users/findoneandupdate', (req, res) => {
+        // Update and return the user object
+        User.findOneAndUpdate({ name: 'Peter' }, { $set: { name: "Sara" } },
+            function (err, user) {
+                if (err) {
+                    console.log(err);
+                }
+                res.send(user);
+            });
+    })
+
+    app.post('users/findbyidandupdate', (req, res) => {
+        User.findByIdAndUpdate(1, { $set: { name: "Sara" } }, function (err) {
+            if (err, user) {
+                console.log(err);
+            }
+            res.send(user);
+        });
+    })
+
+    app.post('users/remove', (req, res) => {
+        User.remove({
+            _id: 1
+        }, function (err) {
+            if (err)
+                res.send(err)
+            res.send({
+                success: true
+            })
+        })
+    })
+
+    app.post('users/findoneandremove', (req, res) => {
+        // return the users object 
+        // can be useful to show a message with the users properties such as name/email
+        // "User with name {x} has been deleted"
+        User.findOneAndRemove({
+            _id: 1
+        }, function (err, user) {
+            if (err)
+                res.send(err)
+            res.send({
+                success: true,
+                user: user
+            })
+        })
+    })
+
+    app.post('users/findbyidandremove', (req, res) => {
+        User.findByIdAndRemove(1, function (err) {
+            if (err)
+                res.send(err)
+            res.send({
+                success: true
             })
         })
     })
--- models/User.js
+++ models/User.js
@@ -3,9 +3,25 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    name: String,
-    email: String
-})
+    name: {
+      type: String,
+      required: [true, 'Let us know you by adding your name']
+    },
+    email: {
+        type: String,
+        required: [true, 'Please add your email as well']
+    },
+    phone: {
+        type: String,
+        required: false,
+        validate: {
+            validator: function(v) {
+            return /\d{3}-\d{3}-\d{4}/.test(v);
+            },
+            message: '{VALUE} is not a valid phone number!'
+            }
+    }
+  });
 
 const User = mongoose.model("User", UserSchema);
 
