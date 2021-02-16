1ab9af10cd2d78c85a5765644bb6bd9278641da5
Brian Palmer <dreamsandcode@gmail.com>
2021-02-15 20:35:28 -0500

    Added put/delete methods

--- controllers/users.js
+++ controllers/users.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser');
 var User = require('../models/User');
 
 var jsonParser = bodyParser.json()
- 
+
 module.exports.controller = (app) => {
 
     app.get('/users', (req, res) => {
@@ -31,6 +31,29 @@ module.exports.controller = (app) => {
                 code: 200,
                 msg: user_resource.name + ' was added'
             })
+        })
+    })
+
+    // update a user
+    app.put('/users/:id', jsonParser, (req, res) => {
+        User.findById(req.params.id, 'name email', function (error, user) {
+            if (error) { console.error(error); }
+            user.name = req.body.name
+            user.email = req.body.email
+            user.save(function (error, user) {
+                if (error) { console.log(error); }
+                res.send(user)
+            })
+        })
+    })
+
+    // delete a user
+    app.delete('/users/:id',jsonParser, (req, res) => {
+        User.remove({
+            _id: req.params.id
+        }, function (error, user) {
+            if (error) { console.error(error); }
+            res.send({ success: true })
         })
     })
 }
